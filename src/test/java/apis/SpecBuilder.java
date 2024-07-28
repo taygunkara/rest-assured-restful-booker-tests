c4fec612d2718cf2575227a4b407ab8db60ae4ab
Taygun Kara <kara.taygun@gmail.com>
2024-07-28 22:09:19 +0300

    feat: SpecBuilder - getReqSpec - getResSpec

---
+++ src/test/java/apis/SpecBuilder.java
@@ -0,0 +1,24 @@
+package apis;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static apis.Routes.BASE_PATH;
+
+public class SpecBuilder {
+
+    public RequestSpecification getReqSpec(){
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_PATH)
+                .build();
+    }
+
+    public ResponseSpecification getResSpec(){
+        return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
+                .build();
+    }
+}
